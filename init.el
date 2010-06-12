df07fe6ef9b159be83195a1c2b27c564f2d89482
Michel Alexandre Salim <michel.salim@informatik.uni-erlangen.de>
2010-06-12 03:36:05 +0200

    Use string-match instead of string-match-p, for Emacs 22 compatibility

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
   (add-to-list 'load-path "~/.emacs.d/emacs22"))
 
 ;; at work
-(when (string-match-p "suse" (version))
+(when (string-match "suse" (version))
   ;; color-theme is installed by hand, not via RPM
   ;; add its location to load path
   (add-to-list 'load-path "~/.emacs.d/color-theme"))
