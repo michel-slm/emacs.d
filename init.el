db05d2593d7d12a1c295b13bc87a7aa99761d49d
Michel Alexandre Salim <michel@michel-slm.name>
2015-09-24 21:39:11 +0700

    Customize shell offset

--- init.el
+++ init.el
@@ -131,22 +131,24 @@
 
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(delete-selection-mode nil)
  '(inhibit-startup-screen t)
  '(org-mobile-directory "~/Desktop/mobile.org")
  '(rpm-spec-user-full-name "Michel Salim")
  '(rpm-spec-user-mail-address "salimma@fedoraproject.org")
- '(scroll-bar-mode (quote right)))
+ '(scroll-bar-mode (quote right))
+ '(sh-basic-offset 2)
+ '(sh-indentation 2))
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 ;;; XQuery
